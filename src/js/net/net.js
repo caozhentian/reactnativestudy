1c50acc421dc867703c45d66927f2358cb246f93
ztcao <ztcao@3ti.us>
2018-04-20 14:53:38 +0800

    网络封装

---
+++ src/js/net/net.js
@@ -0,0 +1,54 @@
+import 'axios-response-logger';
+import axios from 'axios';
+import qs from 'qs';
+
+export default class Net {
+    static BASE_URL = 'http://diting.3tichina.com:80/diting/diting'; 
+    static post(url , data){
+        axios.defaults.baseURL = 'http://diting.3tichina.com:80/diting/diting';    
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded,utf-8';
+        axios.interceptors.request.use(function(config){
+            //在发送请求之前做某事
+            axios.defaults.headers.common['token'] = "djfj2";
+            return config;
+          },function(error){
+            //请求错误时做些事
+            return Promise.reject(error);
+          });
+
+        
+        const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(data),
+        url:url
+        };
+        axios(options)
+        .then(function (response) {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      })
+      .catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+      });   
+    }
+
+}
--- src/js/view/loginview.js
+++ src/js/view/loginview.js
@@ -8,9 +8,7 @@ import {
     View ,
     Alert
 } from 'react-native';
-import 'axios-response-logger';
-import axios from 'axios'
-import qs from 'qs';
+import Net from '../net/net'
 
 /**
  *使用Flexbox布局
@@ -63,50 +61,8 @@ export  default class LoginView extends Component {
 }
 
 function _onPressButton(){
-        axios.defaults.baseURL = 'http://diting.3tichina.com:80/diting/diting';    
-        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded,utf-8';
-        axios.interceptors.request.use(function(config){
-            //在发送请求之前做某事
-            axios.defaults.headers.common['token'] = "djfj2";
-            return config;
-          },function(error){
-            //请求错误时做些事
-            return Promise.reject(error);
-          });
-
-        const data = { 'mobile': '13186076890','code': '1234' , 'IMEI': '893829ABC'};
-        const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        data: qs.stringify(data),
-        url:'/nologin/login'
-        };
-        axios(options)
-        .then(function (response) {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
-      })
-      .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-      });
+    const data = { 'mobile': '13186076890','code': '1234' , 'IMEI': '893829ABC'};
+    Net.post("/nologin/login" ,data)
 }
 const styles = StyleSheet.create({
     container: {
